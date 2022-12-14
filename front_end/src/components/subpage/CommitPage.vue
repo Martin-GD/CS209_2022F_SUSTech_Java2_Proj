9e054a72ae714744b514bcc614dc2f4dfc05c45b
acbgo <1105717400@qq.com>
2022-12-14 23:35:01 +0800

    front_end initial

---
+++ front_end/src/components/subpage/CommitPage.vue
@@ -0,0 +1,85 @@
+<template>
+  <el-container>
+    <el-header style="display: flex; margin-left: 15px">
+      <h2>Release Count: {{ this.totalCount }}</h2>
+    </el-header>
+    <el-main style="margin-top: -30px">
+
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'CommitPage',
+  data () {
+    return {
+      totalCount: 5,
+      releases: [
+        {
+          Id: 1,
+          commitCount: 20
+        },
+        {
+          Id: 2,
+          commitCount: 25
+        },
+        {
+          Id: 3,
+          commitCount: 30
+        },
+        {
+          Id: 4,
+          commitCount: 15
+        },
+        {
+          Id: 5,
+          commitCount: 10
+        }
+      ],
+      commits: [
+        {
+          time: '2022-12-1',
+          count: 10
+        },
+        {
+          time: '2022-12-2',
+          count: 15
+        },
+        {
+          time: '2022-12-3',
+          count: 15
+        },
+        {
+          time: '2022-12-4',
+          count: 20
+        },
+        {
+          time: '2022-12-5',
+          count: 10
+        },
+        {
+          time: '2022-12-6',
+          count: 5
+        },
+        {
+          time: '2022-12-7',
+          count: 13
+        },
+        {
+          time: '2022-12-8',
+          count: 12
+        }
+      ]
+    }
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ front_end/src/components/subpage/DevPage.vue
@@ -0,0 +1,82 @@
+<template>
+  <el-container>
+    <el-header style="display: flex; margin-left: 20px">
+      <h2>Developers Count: {{ this.totalCount }}</h2>
+    </el-header>
+    <el-main style="margin-top: -40px">
+      <el-collapse v-model="activeName" accordion>
+        <el-collapse-item v-for="(item, index) in developers" :name="index" :key="index" @click.native="fillDetail(item)">
+          <template slot="title" style="text-align: center">
+            <span style="margin-left:720px;font-weight:bold;font-size:20px;color:#2C8DF4;">{{ item.login }}</span>
+          </template>
+          <el-container>
+            <el-aside class="Avatar">
+              <img :src="imageUrl" style="width: 80px; border-radius: 50%; margin-top: 30px">
+            </el-aside>
+            <el-main class="Detail">
+              <el-table :data="singleDev" border style="border-radius: 15px"
+                        :header-cell-style="{'background-color': '#1989fa', 'color': '#fff', 'font-size': '15px'}"
+                        :row-style="{'background-color':'#ecf5ff', 'font-size': '13x'}">
+                <el-table-column prop="login" label="UserName" style="font-size: 15px"></el-table-column>
+                <el-table-column prop="id" label="ID" style="font-size: 15px"></el-table-column>
+                <el-table-column prop="ownerRepo" label="Repo" style="font-size: 15px"></el-table-column>
+                <el-table-column prop="contributions" label="Contributions" style="font-size: 15px"></el-table-column>
+                <el-table-column prop="type" label="Type" style="font-size: 15px"></el-table-column>
+              </el-table>
+            </el-main>
+          </el-container>
+        </el-collapse-item>
+      </el-collapse>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'DevPage',
+  data () {
+    return {
+      activeName: '1',
+      imageBaseUrl: 'https://avatars.githubusercontent.com/',
+      imageUrl: 'https://avatars.githubusercontent.com/',
+      totalCount: 10,
+      developers: [
+        {
+          ownerRepo: 'project',
+          login: 'acbgo',
+          id: '12345',
+          contributions: 50,
+          type: 'good'
+        },
+        {
+          ownerRepo: 'hello world',
+          login: 'Martin-GD',
+          id: '0987',
+          contributions: 40,
+          type: 'bad'
+        }
+      ],
+      singleDev: []
+    }
+  },
+  mounted () {
+  },
+  methods: {
+    fillDetail (item) {
+      this.singleDev = []
+      this.singleDev.push(item)
+      this.imageUrl = this.imageBaseUrl + item.login
+    }
+  }
+}
+</script>
+
+<style scoped>
+.Avatar {
+  margin-left: -50px;
+}
+
+.Detail {
+  margin-left: -70px;
+}
+</style>
